203e47ad49a00cf55aaa614c404c28f80079c28c
Leandro Soares dos Reis <55002801+leansdr@users.noreply.github.com>
2021-06-28 20:14:59 -0300

    incluido login e register

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,65 +2,65 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title')</title>
+  <title>@yield('title')</title>
 
-    <!-- Fonte do Google -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+  <!-- Fonte do Google -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
-    <!-- CSS Boostrap    -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <!-- CSS Boostrap    -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
-    <!-- CSS da aplicação -->
-    <link rel="stylesheet" href="/css/styles.css">
-    <script src="/js/scripts.js"></script>
+  <!-- CSS da aplicação -->
+  <link rel="stylesheet" href="/css/styles.css">
+  <script src="/js/scripts.js"></script>
 
 </head>
 
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="collapse navbar-collapse" id="navbar">
-            <a href="/" class="navbar-brand">
-                <img src="/img/hdcevents_logo.svg" alt="Logo">
-            </a>
-            <ul class="navbar-nav text-white">
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Eventos</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/events/create" class="nav-link">Criar Eventos</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Entrar</a>
-                </li>
-                <li class="nav-item">
-                    <a href="/" class="nav-link">Cadastrar</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="collapse navbar-collapse" id="navbar">
+      <a href="/" class="navbar-brand">
+        <img src="/img/hdcevents_logo.svg" alt="Logo">
+      </a>
+      <ul class="navbar-nav text-white">
+        <li class="nav-item">
+          <a href="/" class="nav-link">Eventos</a>
+        </li>
+        <li class="nav-item">
+          <a href="/events/create" class="nav-link">Criar Eventos</a>
+        </li>
+        <li class="nav-item">
+          <a href="/login" class="nav-link">Entrar</a>
+        </li>
+        <li class="nav-item">
+          <a href="/register" class="nav-link">Cadastrar</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </header>
 
 <body>
-    <main class="container-fluid">
-        <div class="row">
-            @if(session('msg'))
-            <p class="msg">{{session('msg')}}</p>
-            @endif
-            @yield('content')
-        </div>
-    </main>
-    <footer>
-        <p>HDC Events &copy; 2021</p>
-    </footer>
-    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
-    <!--<script src="https://unpkg.com/ionicons@5.5.1/dist/ionicons.js"></script>-->
+  <main class="container-fluid">
+    <div class="row">
+      @if(session('msg'))
+      <p class="msg">{{session('msg')}}</p>
+      @endif
+      @yield('content')
+    </div>
+  </main>
+  <footer>
+    <p>HDC Events &copy; 2021</p>
+  </footer>
+  <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+  <!--<script src="https://unpkg.com/ionicons@5.5.1/dist/ionicons.js"></script>-->
 </body>
 
 </html>
